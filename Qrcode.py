64f7c66d3fc334d822cc6c931356da2ff4e41835
Shruti Kodinariya <shrutikodinaria@gmail.com>
2022-04-09 14:17:32 +0530

    First

---
+++ Qrcode.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import streamlit.components.v1 as components
+from PIL import Image
+import qrcode
+
+html_code = """
+        <div style="background-color: #1abc9c; padding:  10px; border-radius: 10px">
+          <h1 style="color:white; text-align: center">QR Code</h1>
+        </div>
+        """
+components.html(html_code)
+
+# Image
+img = Image.open("qrcode.jpg")
+st.image(img, width=700)
+
+data = st.text_input("ENTER THE LINK : ")
+
+box_size = st.number_input("ENTER BOX SIZE :")
+border = st.number_input("ENTER BORDER SPACING :")
+
+from matplotlib import colors as mcolors
+
+colors = mcolors.CSS4_COLORS
+
+colors_list = list(colors.keys())
+
+fill = st.selectbox("SELECT THE FILL COLOR : ", colors_list)
+back = st.selectbox("SELECT THE BACK COLOR : ", colors_list)
+
+if (st.button("GENERATE QR CODE")):
+    qr = qrcode.QRCode(box_size=box_size, border=border)
+
+    qr.add_data(data)
+    qr.make()
+
+    img = qr.make_image(fill_color=fill,
+                        back_color=back)
+    img.save('myqr.png')
+    img = Image.open("myqr.png")
+    st.image(img)
